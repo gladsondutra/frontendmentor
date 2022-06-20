2d0835babf85a7b484b27c3dd53bc9edc9f3b9b1
Gladson Dutra <gladsonddutra@gmail.com>
2022-06-20 17:30:27 -0300

    update

--- 01-qr-code-component-main/src/index.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-bg');
-
-toggleBtn.addEventListener('click', function () {
-  this.classList.toggle('active');
-  document.body.classList.toggle('darkTheme')
-})
